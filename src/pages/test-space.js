1bd2cb0a7cac3d8969a6e50cbb668f7efb65ad52
2himitu <hmhnes@gmail.com>
2022-05-31 21:04:17 +0900

    test,test-space추가

---
+++ src/pages/test-space.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function testSpacePage(){
+  return(
+    <main>
+      <title>testSpacePage</title>
+      <h1>testSpacePage입니다.</h1>
+    </main>
+  );
+}
+export default testSpacePage;
---
+++ src/pages/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function testPage(){
+  return(
+    <main>
+      <title>testPage</title>
+      <h1>testPage입니다.</h1>
+    </main>
+  );
+}
+export default testPage;
