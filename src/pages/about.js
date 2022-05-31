7ccc1da667868ef20622a46e77d8dfbc54a101a4
2himitu <hmhnes@gmail.com>
2022-05-31 20:25:57 +0900

    AboutPage추가

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutPage(){
+  return (
+    <main>
+      <title>AboutPage</title>
+      <h1>AboutPage</h1>
+    </main>
+  );
+}
+export default AboutPage;
