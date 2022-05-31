8694a99f272bb44bc454abb9923edb6bca5a3d5e
2himitu <hmhnes@gmail.com>
2022-05-31 22:06:37 +0900

    layout수정 및 vision 추가

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,3 +19,4 @@ function Layout({Children}){
   );
 
 }
+export default Layout;
---
+++ src/pages/vision.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+function visionPage(){
+  return(
+    <Layout>
+
+      <title>
+        <h1>비전 페이지입니다.</h1>
+      </title>
+    </Layout>
+  );
+}
