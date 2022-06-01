9c0dbe28d454bd759f40d185c0cd875f928a6cf9
2himitu <hmhnes@gmail.com>
2022-06-01 20:28:12 +0900

    블로그 글 리스트에서 상세글로 링크

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,13 +1,15 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import Layout from "../components/layout";
+import Layout from "../../components/layout";
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+          <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
           <MDXRenderer>{node.body}</MDXRenderer>
         </article>
@@ -25,6 +27,7 @@ export const query = graphql`
         }
         id
         body
+        slug
       }
     }
   }
